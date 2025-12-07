25382976c8bc94883cf01f73c61cc7f94b9b812f
Gr-be <greyson.beckford28@houghton.edu>
2025-12-06 21:16:40 -0500

    Signin&Login FINAL VERSION
    Fixed the sign-in and log-in functions to work with the user table instead of passenger

--- UserSigninLogin.js
+++ UserSigninLogin.js
@@ -1,6 +1,6 @@
-let mysql = require('mysql');
+let EastWestDB = require('mysql');
 
-let dbconn =  mysql.createConnection({
+let dbconn =  EastWestDB.createConnection({
     host: "localhost",
     user: "root",
     password: "poo.py",
@@ -8,50 +8,50 @@ let dbconn =  mysql.createConnection({
 
 });
 
-dbconn.connect(function(err) {
-    if (err) throw err;
+dbconn.connect(function(error) {
+    if (error) throw error;
     console.log("Connected");
 });
 
 
 // Function for user sign-in
-function userSignIn(name, email, password) {
-    const sign_in_prompt = "insert into passenger(pass_name, pass_email, password) values(?, ?, ?)";
-    let email_search = "select * from passenger where pass_email = ?";
+function userSignUp(first_name,last_name, user_email, password) {
+    const sign_in_prompt = "insert into user(fname, lname, user_email, user_password) values(?, ?, ?, ?)";
+    let email_search = "select * from user where user_email = ?";
 
 //Checks to see if email already exists
-    dbconn.query(email_search, [email], function(err, results) {
-        if (err) throw err;
+    dbconn.query(email_search, [user_email], function(error, results) {
+        if (error) throw error;
 
         if (results.length > 0) {
-            console.log("Email already registered.");
+            return ("Email already registered.");
         } 
         
         else {
 //Registers new user
-            dbconn.query(sign_in_prompt, [name, email, password], function(err, results) {
-                if (err) throw err;
-                console.log("User registered successfully.");
+            dbconn.query(sign_in_prompt, [first_name, last_name, user_email, password], function(error, results) {
+                if (error) throw error;
+                return ("User registered successfully.");
         
             });
         }
     });
 }
 
-//userSignIn("Jim Timson", "jimmyt@outlook.com", "poo.py_file"); This is a test function call.
- 
-// Function for user login
-function userLogin(email, password) {
-    const login_prompt = "select * from passenger where pass_email = ? and password = ?";
 
-    //Checks to see if the account exists
-    dbconn.query(login_prompt, [email, password], function(err, results) {
-        if (err) throw err;
+// Function for user login
+function userLogin(user_email, user_password) {
+    const login_prompt = "select * from user where user_email = ? and user_password = ?";
+
+//Checks to see if the account actually exists
+    dbconn.query(login_prompt, [user_email, user_password], function(error, results) {
+        if (error) throw error;
 
         if (results.length > 0) {
             console.log("Login successful.");
-            return ("Welcome, " + results[0].pass_name + "!");  
+            return ("Welcome, " + results[0].fname, results[0].lname + "!");  
         } 
+
         
         else {
             console.log("Invalid email or password.");
@@ -60,4 +60,4 @@ function userLogin(email, password) {
 }
 
 
-//console.log(userLogin("jimmyt@outlook.com","poo.py_file"));
+//userLogin("jimmyt@outlook.com", "poo.py");
